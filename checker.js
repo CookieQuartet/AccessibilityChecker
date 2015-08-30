5105176b0af1a4a192a1177f6b81476ced8a80fd
Martin Maestri <mmaestri@gmail.com>
2015-08-30 20:34:34 -0300

    Se agrega código para procesar XML

--- checker.js
+++ checker.js
@@ -1,10 +1,71 @@
-//var platform = 'android/java';
-var platform = 'android/xml';
-var options = require('./platform/' + platform + '/options');
-var arg = process.argv[2] ;
-//var filename = typeof arg !== 'undefined' ? arg : 'platform/android/java/test/BeGenerousActivity.java';
-var filename = 'platform/android/xml/test/sample_main.xml';
-var srcProc = require('./core/SourceProcessor');
-var output = srcProc(filename, options);
+/*
+ Parámetros de checker
+ -p <platform>: plataforma de análisis
+      Opciones:
+      * android/xml
+      * android/java
+ -f <file>: nombre de archivo
+      Ejemplos:
+      * 'platform/android/java/test/BeGenerousActivity.java';
+      * 'platform/android/xml/test/sample_main.xml';
 
-console.log(output);
+ -x <project>: nombre de proyecto
+ */
+//----------------------------------------------------------------------------------------------------------------
+var _ = require('lodash'),
+    fs = require('fs'),
+    srcProc,
+    output,
+    argIndex,
+    platform,
+    filename,
+    options;
+
+//----------------------------------------------------------------------------------------------------------------
+argIndex = {
+  platform: process.argv.indexOf('-p') + 1,
+  project: process.argv.indexOf('-x') + 1,
+  filename: process.argv.indexOf('-f') + 1
+};
+//----------------------------------------------------------------------------------------------------------------
+platform = argIndex.platform > 0 ? argIndex.platform  : 'android/xml';
+//----------------------------------------------------------------------------------------------------------------
+filename = argIndex.filename > 0 ? process.argv[argIndex.filename] : '';
+//----------------------------------------------------------------------------------------------------------------
+project = argIndex.project > 0 ? process.argv[argIndex.project] : '';
+//----------------------------------------------------------------------------------------------------------------
+try {
+  options = require('./platform/' + platform + '/options');
+} catch(e) {
+  options = require('./platform/android/xml/options');
+}
+//----------------------------------------------------------------------------------------------------------------
+srcProc = require('./core/SourceProcessor');
+//----------------------------------------------------------------------------------------------------------------
+if(project.length > 0) {
+  fs.lstat(project, function(err, stats) {
+    if (!err && stats.isDirectory()) {
+      // Yes it is
+    }
+  });
+} else if(filename.length > 0) {
+  fs.lstat(filename, function(err, stats) {
+    if (!err && stats.isFile()) {
+      try {
+        output = srcProc(filename, options);
+        console.log(output);
+      } catch(e) {
+        if(_.isObject(options)) {
+          output = 'Error: no es posible encontrar la configuración seleccionada\n';
+        }
+        if(filename.length == 0) {
+          output = 'Error: no se ha seleccionado un archivo\n';
+        }
+      }
+    } else {
+      output = 'Error: no existe el archivo\n';
+    }
+  });
+}
+//----------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
--- platform/android/xml/XMLCodeRules.js
+++ platform/android/xml/XMLCodeRules.js
@@ -1,4 +1,4 @@
-var JavaCodeRules = [
+var XMLCodeRules = [
   {
     id: 1,
     priority: 1,
@@ -11,21 +11,8 @@ var JavaCodeRules = [
       var comment = '\n<!-- esto es un comentario de tag -->\n';
       return code.substring(0, startToken+1) + comment + code.substring(startToken+1, code.length);
     }
-  } ,
-  {
-    id: 2,
-    priority: 0,
-    name: 'Regla 2',
-    description: 'Regla de prueba de atributo',
-    type: 'android.xml.attribute',
-    match: function(code) { return true; },
-    apply: function(code, ctx) {
-      var startToken = code.indexOf('{');
-      var comment = '\n// esto es un comentario de clase\n';
-      return code.substring(0, startToken+1) + comment + code.substring(startToken+1, code.length);
-    }
   }
 
 ];
 
-module.exports = JavaCodeRules;
+module.exports = XMLCodeRules;
