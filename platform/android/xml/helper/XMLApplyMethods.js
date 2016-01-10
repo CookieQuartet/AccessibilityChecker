f43325aebc846b4c0c659841adf9a7fd4d9b20e3
mauricio lópez <mauricio lpez>
2016-01-09 22:13:30 -0300

    apply de las unidades

--- platform/android/xml/helper/XMLApplyMethods.js
+++ platform/android/xml/helper/XMLApplyMethods.js
@@ -5,13 +5,26 @@ module.exports = {
             return code.replace("android:layout_width="+applyObject.quote+applyObject.size, widthString);
         },
         NoWidthDp: function(code, applyObject){
-            return code;
+            return code.replace(
+                code.substring(
+                    code.match("android:layout_width")[0].length,
+                    code.length
+                ).match(applyObject.unit),
+                "dp"
+            )
         },
         HeightSize: function(code, applyObject){
-            return code;
+            return code.replace(
+                code.substring(
+                    code.match("android:layout_height")[0].length,
+                    code.length
+                ).match(applyObject.unit),
+                "dp"
+            )
         },
         NoHeightDp: function(code, applyObject){
-            return code;
+            var widthString = "android:layout_width="+applyObject.quote+"48";
+            return code.replace("android:layout_height="+applyObject.quote+applyObject.size, widthString);
         }
     }
 }
