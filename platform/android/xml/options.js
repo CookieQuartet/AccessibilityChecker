c2ac34ad72530dca1ceb3c861123cf4ffcde598c
Julian Falcionelli <julianfalcionelli@gmail.com>
2016-06-24 18:13:41 -0300

    Ignore rute

--- platform/android/xml/options.js
+++ platform/android/xml/options.js
@@ -2,7 +2,7 @@ var options = {
   //file: 'platform/android/java/test/BeGenerousActivity.java',
   platform: 'android/xml/',
   fileExtension: 'xml',
-  ignore: ['/*/.idea/**', '/*/app/build/**', '/**/build/**', 'build/**'],
+  ignore: ['/*/.idea/**', '/*/app/build/**', '/**/build/**', 'build/**', 'app/build/**'],
   parser: 'XMLParser',
   lexer: 'XMLLexer',
   listener: 'XMLParserListener',
